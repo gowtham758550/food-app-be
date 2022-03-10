e3e284dfe1fb06ac40309cdc937263b25c129f14
naman0058 <jnaman345@gmail.com>
2022-03-10 10:21:01 +0530

    a

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var pool =  require('./pool');
+var upload = require('./multer');
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
